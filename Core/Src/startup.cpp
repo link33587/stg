d74ab907d82c8ee2c5610043ae948b6c2538835f
link33587 <1683609319@qq.com>
2026-05-17 14:32:09 +0800

    Update startup.cpp

--- Core/Src/startup.cpp
+++ Core/Src/startup.cpp
@@ -19,30 +19,6 @@ void init()
 	initGame();
 }
 
-/*
-基于STM32的飞机大战小游戏
-效果演示视频链接：https://www.bilibili.com/video/BV1HoxfeFECU/
-
-游戏效果可以 关注 微信公众号、B站、抖音和快手等平台（账号名称：JL单片机），有录制的视频的效果。
-也分享了其他好玩有趣的代码，都可以去微信公众号下载。感兴趣的可以看下。
-
-
-材料：
-STM32F103C8T6最小系统板
-SSD1306 OLED128*64显示屏
-6个按键
-
-IO接线说明：
-向下按键：PA4
-向右按键：PA5
-向上按键：PB11
-向左按键：PB10
-A按键：PA6
-B按键：PA7
-
-OLED SCK：PA11
-OLED SDA：PA12
-*/
 void startup()
 {
 	init();
